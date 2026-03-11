e3753098c2432457e581ad9636ebadcd1eb002cd
Gabezerra12 <bezeba02@gmail.com>
2026-03-11 08:36:58 -0300

    atualização de dados

--- src/Application/Service/user_service.py
+++ src/Application/Service/user_service.py
@@ -4,8 +4,13 @@ from src.config.data_base import db
 
 class UserService:
     @staticmethod
-    def create_user(name, email, password):        
-        user = User(name=name, email=email, password=password)        
+    def create_user(name, cnpj, email, celular, password):
+        user = User(name=name, cnpj=cnpj, email=email, celular=celular, password=password, status='inactive')
         db.session.add(user)
-        db.session.commit()       
-        return UserDomain(user.id, user.name, user.email, user.password)
+        db.session.commit()
+        return UserDomain(user.id, user.name, user.cnpj, user.email, user.celular, user.password, user.status)
+
+    @staticmethod
+    def _generate_activation_code():
+        from random import randint
+        return f"{randint(1000, 9999)}"
